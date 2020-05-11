ab10840cf255c8246469b00b0504d5c816d7fc85
Noele Ribeiro <noelemribeiro@gmail.com>
2020-05-11 20:50:34 -0300

    Initial commit

---
+++ R/colar.R
@@ -0,0 +1,15 @@
+
+#' Cola dois numeros
+#'
+#' Recebe os parametros x e y e retorna eles colados, com um espaco de separacao
+#'
+#' @param x primeiro argumento
+#' @param y segundo argumento
+#'
+#' @return os paramentros colados
+#'
+#' @export
+colar = function(x,y) {
+  paste(x,y)
+}
+
